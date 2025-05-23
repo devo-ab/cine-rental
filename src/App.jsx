2dea9cc66a1a25911c4615f950cb68b59c45703f
devo-ab <per.useravi0@gmail.com>
2025-05-23 22:43:30 +0600

    added movie card and show movie deatils in UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import MovieList from "./cine/MovieList";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 
@@ -5,9 +7,11 @@ function App() {
   return (
     <div className="max-w-7xl mx-auto">
       <Header></Header>
-      <div class="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+      <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
         <Sidebar></Sidebar>
+        <MovieList></MovieList>
       </div>
+      <Footer></Footer>
     </div>
   );
 }
