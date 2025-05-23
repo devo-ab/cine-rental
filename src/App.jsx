cb771400980a22c5c026ac9ce01f826164686c0d
devo-ab <per.useravi0@gmail.com>
2025-05-23 22:06:15 +0600

    added header and side bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
-    <>
-      <div className="text-green-500">Cine Rental</div>
-    </>
+    <div className="max-w-7xl mx-auto">
+      <Header></Header>
+      <div class="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+        <Sidebar></Sidebar>
+      </div>
+    </div>
   );
 }
 
